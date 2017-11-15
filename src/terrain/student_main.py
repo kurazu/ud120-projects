df1d1cd66aa216240a578940bf9ac9e6503c96c4
Tomasz Maćkowiak <kurazu@kurazu.net>
2017-11-15 18:29:57 +0100

    cleanup

--- src/terrain/student_main.py
+++ src/terrain/student_main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """ Complete the code in ClassifyNB.py with the sklearn
     Naive Bayes classifier to classify the terrain data.
 
@@ -12,29 +10,44 @@ from .prep_terrain_data import makeTerrainData
 from .class_vis import prettyPicture
 from .classify import classify
 
-import numpy as np
-import pylab as pl
+# import numpy as np
+# import pylab as pl
 
 
 def main():
-    features_train, labels_train, features_test, labels_test = makeTerrainData()
+    features_train, labels_train, features_test, labels_test = \
+        makeTerrainData()
 
-    ### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
-    ### in together--separate them so we can give them different colors in the scatterplot,
-    ### and visually identify them
-    grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-    bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-    grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-    bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+    # the training data (features_train, labels_train)
+    # have both "fast" and "slow" points mixed
+    # in together--separate them so we can give them different
+    # colors in the scatterplot,
+    # and visually identify them
 
+    # grade_fast = [
+    #     features_train[ii][0]
+    #     for ii in range(0, len(features_train))
+    #     if labels_train[ii] == 0
+    # ]
+    # bumpy_fast = [
+    #     features_train[ii][1]
+    #     for ii in range(0, len(features_train))
+    #     if labels_train[ii] == 0
+    # ]
+    # grade_slow = [
+    #     features_train[ii][0]
+    #     for ii in range(0, len(features_train))
+    #     if labels_train[ii] == 1
+    # ]
+    # bumpy_slow = [
+    #     features_train[ii][1]
+    #     for ii in range(0, len(features_train))
+    #     if labels_train[ii] == 1
+    # ]
 
-    # You will need to complete this function imported from the ClassifyNB script.
-    # Be sure to change to that code tab to complete this quiz.
     clf = classify(features_train, labels_train)
 
-
-
-    ### draw the decision boundary with the text points overlaid
+    # draw the decision boundary with the text points overlaid
     prettyPicture(clf, features_test, labels_test)
 
 
