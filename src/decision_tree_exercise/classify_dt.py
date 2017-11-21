40b226433c9f2a2cfb0c125f7dd355de1b2c8175
Tomasz Maćkowiak <kurazu@kurazu.net>
2017-11-21 19:01:07 +0100

    dt

--- src/decision_tree_exercise/classify_dt.py
+++ src/decision_tree_exercise/classify_dt.py
@@ -2,6 +2,6 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 def classify(features_train, labels_train):
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(min_samples_split=2)
     clf.fit(features_train, labels_train)
     return clf
