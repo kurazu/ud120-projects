78577ca2937bad0ca1cf1103c21ec6c862af77ae
Tomasz Maćkowiak <kurazu@kurazu.net>
2017-12-29 16:22:40 +0100

    validation

--- src/validation/validate_poi.py
+++ src/validation/validate_poi.py
@@ -10,23 +10,35 @@
     After that, it's not our code anymore--it's yours!
 """
 
-import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+import os.path
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+from tools.feature_format import featureFormat, targetFeatureSplit
+from tools.loading import load_pickle
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
-features_list = ["poi", "salary"]
-
-data = featureFormat(data_dict, features_list)
-labels, features = targetFeatureSplit(data)
+import final_project
+import tools
 
 
+def main():
+    keys_path = os.path.join(
+        os.path.dirname(tools.__file__),
+        'python2_lesson13_keys.pkl'
+    )
 
-### it's all yours from here forward!  
+    data_dict = load_pickle(os.path.join(
+        os.path.dirname(final_project.__file__), 'final_project_dataset.pkl'
+    ))
+
+    # first element is our labels, any added elements are predictor
+    # features. Keep this the same for the mini-project, but you'll
+    # have a different feature list when you do the final project.
+    features_list = ["poi", "salary"]
+
+    data = featureFormat(data_dict, features_list, sort_keys=keys_path)
+    labels, features = targetFeatureSplit(data)
+
+    # it's all yours from here forward!
 
 
+if __name__ == '__main__':
+    main()
